44e8842a814ad6a72fc88dba43c7b103dcd44a03
Marko Jekrić <markojerkic266@gmail.com>
2024-02-10 21:40:06 +0100

    nekakve konekcije

--- client/main.go
+++ client/main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func getAvailableClients() ([]string, error) {
@@ -33,12 +35,91 @@ func getAvailableClients() ([]string, error) {
 	return availableClients, nil
 }
 
+func promptForClient(availableClients []string, myPort int) string {
+	var selectedClient string
+
+	for {
+
+		for index, client := range availableClients {
+			if client == fmt.Sprintf("%d", myPort) {
+				continue
+			}
+
+			fmt.Println(index+1, ": ", client)
+		}
+
+		fmt.Print("Select client: ")
+		fmt.Scanln(&selectedClient)
+
+		selectedClientIndex := int(selectedClient[0]-'0') - 1
+
+		if selectedClientIndex < 0 || selectedClientIndex >= len(availableClients) {
+			fmt.Println("Invalid index")
+			continue
+		}
+
+		return availableClients[selectedClientIndex]
+	}
+}
+
+func connectToClient(port string) {
+
+	fmt.Println("Connecting to client", fmt.Sprintf("http://localhost:%s/connect", port));
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://localhost:%s/connect", port), nil)
+
+	if err != nil {
+		log.Println("Error connecting to client", err)
+		return
+	}
+
+	go func() {
+		fmt.Println("Enter message: ")
+		for {
+			var message string
+			fmt.Scanln(&message)
+
+			err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+			if err != nil {
+				log.Println("Error writing message", err)
+				return
+			}
+		}
+	}()
+
+}
+
 func main() {
 
 	log.Println("Starting client")
 
 	http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Connection request received", r.RemoteAddr)
+
+		upgrader := websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		}
+
+		conn, err := upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			log.Println("Error upgrading connection", err)
+			return
+		}
+
+		go func() {
+			defer conn.Close()
+			for {
+				_, message, err := conn.ReadMessage()
+				if err != nil {
+					log.Println("Error reading message", err)
+					return
+				}
+				log.Printf("Received message: %s", message)
+
+			}
+		}()
+
 	})
 
 	server := &http.Server{
@@ -52,7 +133,8 @@ func main() {
 		panic(err)
 	}
 
-	requestBody := fmt.Sprintf("address=%d", listener.Addr().(*net.TCPAddr).Port)
+	myPort := listener.Addr().(*net.TCPAddr).Port
+	requestBody := fmt.Sprintf("address=%d", myPort)
 
 	response, err := http.Post("http://localhost:8080/register", "application/x-www-form-urlencoded", bytes.NewBuffer([]byte(requestBody)))
 
@@ -72,7 +154,13 @@ func main() {
 		panic(err)
 	}
 
-	log.Println("Available clients", clients)
+	go func() {
+		selectedClient := promptForClient(clients, myPort)
+
+		connectToClient(selectedClient)
+
+		log.Println("Selected client:", selectedClient)
+	}()
 
 	panic(http.Serve(listener, nil))
 
